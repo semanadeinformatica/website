cc98342e52d51414cd4214155ecb1b8974148368
toni-santos <a.oliveirasantos122@gmail.com>
2023-08-08 19:28:04 +0100

    setup inertia for error pages and create maintenance page

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,9 +4,38 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Inertia\Inertia;
 
 class Handler extends ExceptionHandler
 {
+
+    /**
+     * Prepare exception for rendering.
+     *
+     * @param  \Throwable  $e
+     * @return \Throwable
+     */
+    public function render($request, Throwable $e)
+    {
+        $response = parent::render($request, $e);
+        // ! app()->environment(['local', 'testing']) &&
+        if (in_array($response->status(), [500, 404, 403])) {
+            return Inertia::render('Error', ['status' => $response->status()])
+                ->toResponse($request)
+                ->setStatusCode($response->status());
+        } elseif ($response->status() === 503) {
+            return Inertia::render('Maintenance')
+                ->toResponse($request)
+                ->setStatusCode($response->status());
+        } elseif ($response->status() === 419) {
+            return back()->with([
+                'message' => 'The page expired, please try again.',
+            ]);
+        }
+
+        return $response;
+    }
+
     /**
      * The list of the inputs that are never flashed to the session on validation exceptions.
      *
---
+++ resources/js/Pages/Error.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({ status: Number })
+
+const title = computed(() => {
+  return {
+    500: '500: Server Error',
+    404: '404: Page Not Found',
+    403: '403: Forbidden',
+  }[props.status]
+})
+
+const description = computed(() => {
+  return {
+    500: 'Whoops, something went wrong on our servers.',
+    404: 'Sorry, the page you are looking for could not be found.',
+    403: 'Sorry, you are forbidden from accessing this page.',
+  }[props.status]
+})
+</script>
+
+<template>
+  <div>
+    <H1>{{ title }}</H1>
+    <div>{{ description }}</div>
+  </div>
+</template>
