3c73fac3e37af78f78a6f88996e0c92c5190d7e3
pedronunes19 <pedromiguelmnunes@gmail.com>
2023-08-21 18:20:58 +0100

    add footer

---
+++ resources/js/Components/Footer.vue
@@ -0,0 +1,46 @@
+<script setup>
+</script>
+
+<template>
+    <footer class="bg-2023-bg py-4">
+        <section class="flex content-center items-center justify-center relative p-8 mx-[20%]">
+            <div class="flex gap-2 flex-row items-center w-full justify-evenly max-[900px]:hidden">
+                <a class="p-2.5 rounded-full flex w-fit bg-2023-red" target="_blank" href="https://www.facebook.com/NIAEFEUP">
+                    <v-icon name="io-logo-facebook" fill="white" scale="1.3"></v-icon>
+                </a>
+                <a class="p-2.5 rounded-full flex w-fit bg-2023-red-dark" target="_blank" href="https://www.linkedin.com/company/nifeup/mycompany/">
+                    <v-icon name="io-logo-linkedin" fill="white" scale="1.3"></v-icon>
+                </a>
+                <a class="p-2.5 rounded-full flex w-fit bg-2023-teal" target="_blank" href="https://www.instagram.com/niaefeup/">
+                    <v-icon name="io-logo-instagram" fill="white" scale="1.3"></v-icon>
+                </a>
+                <a class="p-2.5 rounded-full flex w-fit bg-2023-orange" target="_blank" href="mailto:ni@aefeup.pt">
+                    <v-icon name="io-mail" fill="white" scale="1.3"></v-icon>
+                </a>
+            </div>
+            <div class="hidden flex-col items-center gap-2 max-[900px]:flex">
+                <div class="grid grid-cols-2 gap-2 justify-center">
+                    <a class="p-2.5 rounded-full flex w-fit bg-2023-red" target="_blank" href="https://www.facebook.com/NIAEFEUP">
+                        <v-icon name="io-logo-facebook" fill="white" scale="1.3"></v-icon>
+                    </a>
+                    <a class="p-2.5 rounded-full flex w-fit bg-2023-red-dark" target="_blank" href="https://www.linkedin.com/company/nifeup/mycompany/">
+                        <v-icon name="io-logo-linkedin" fill="white" scale="1.3"></v-icon>
+                    </a>
+                    <a class="p-2.5 rounded-full flex w-fit bg-2023-teal" target="_blank" href="https://www.instagram.com/niaefeup/">
+                        <v-icon name="io-logo-instagram" fill="white" scale="1.3"></v-icon>
+                    </a>
+                    <a class="p-2.5 rounded-full flex w-fit bg-2023-orange" target="_blank" href="mailto:ni@aefeup.pt">
+                        <v-icon name="io-mail" fill="white" scale="1.3"></v-icon>
+                    </a>    
+                </div>
+            </div>
+        </section>  
+        <div class="container mx-auto text-center font-bold text-2023-teal">
+            <p>Copyright &copy; 2023 niaefeup, todos os direitos reservados</p>
+        </div>
+    </footer>
+
+</template>
+
+<style>
+</style>
