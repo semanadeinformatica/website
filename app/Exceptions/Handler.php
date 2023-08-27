c49d87379176f21fa39b6fa807ef9f382acedb10
toni-santos <a.oliveirasantos122@gmail.com>
2023-08-27 16:23:53 +0100

    fix missing images and faulty env logic

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -26,7 +26,7 @@ class Handler extends ExceptionHandler
             return Inertia::render('Error', ['status' => $response->status()])
                 ->toResponse($request)
                 ->setStatusCode($response->status());
-        } elseif ($response->status() === 503 && !app()->environment(['maintenance'])) {
+        } elseif ($response->status() === 503 && app()->environment(['maintenance'])) {
             return Inertia::render('Maintenance')
                 ->toResponse($request)
                 ->setStatusCode($response->status());
--- resources/js/Pages/Maintenance.vue
+++ resources/js/Pages/Maintenance.vue
@@ -4,12 +4,12 @@
 <template>
   <main class="min-h-screen bg-2023-bg">
     <div class="flex flex-col gap-[60px] py-[65px] justify-evenly items-center content-center min-h-screen">
-                <img id="svg-1" class="animate-2023-maintenance-jump opacity-50 absolute max-[900px]:hidden w-[150px] left-[7%]" src="storage/images/cy-sinf.svg" alt="Stylized SINF logo">
-                <img id="svg-2" class="animate-2023-maintenance-jump opacity-50 absolute max-[900px]:hidden top-[70px] w-[120px] right-[7%]" src="storage/images/rc-sinf.svg" alt="Stylized SINF logo">
-                <img id="svg-3" class="animate-2023-maintenance-jump opacity-50 absolute max-[900px]:hidden bottom-[100px] w-[90px] right-[15%]" src="storage/images/oc-sinf.svg" alt="Stylized SINF logo">
+                <img id="svg-1" class="animate-2023-maintenance-jump opacity-50 absolute max-[900px]:hidden w-[150px] left-[7%]" src="images/cy-sinf.svg" alt="Stylized SINF logo">
+                <img id="svg-2" class="animate-2023-maintenance-jump opacity-50 absolute max-[900px]:hidden top-[70px] w-[120px] right-[7%]" src="images/rc-sinf.svg" alt="Stylized SINF logo">
+                <img id="svg-3" class="animate-2023-maintenance-jump opacity-50 absolute max-[900px]:hidden bottom-[100px] w-[90px] right-[15%]" src="images/oc-sinf.svg" alt="Stylized SINF logo">
                 
                 <div class="relative">
-                    <img class="w-[450px] max-[900px]:w-[300px]" src="storage/images/sinf logo.png" alt="Stylized SINF logo">
+                    <img class="w-[450px] max-[900px]:w-[300px]" src="images/sinf logo.png" alt="Stylized SINF logo">
                     <span class="text-2023-teal text-xl font-bold absolute bottom-[-20px] right-0 margin-0">2023</span>
                 </div>
                 <p class="shadow-md shadow-2023-teal text-2023-teal p-2.5 border border-solid border-black px-8 text-lg font-bold mr-2">semana_de_informática</p>
@@ -17,7 +17,7 @@
                 <p class="text-2023-teal text-lg font-bold m-0 max-[900px]:text-base">Estamos a preparar mais uma edição da SINF!</p>
                 <a href="https://sinf.pt" class="text-2023-teal no-underline text-lg font-bold m-0 max-[900px]:text-base relative">
                     Revê a edição do último ano aqui!
-                    <img class="w-3 absolute bottom-[-5px] right-[5px]" src="storage/images/pointer.png" alt="Pointer">
+                    <img class="w-3 absolute bottom-[-5px] right-[5px]" src="images/pointer.png" alt="Pointer">
                 </a>
                 <section class="flex content-center items-center justify-center relative p-8 border border-black border-solid mx-[25%]">
                     <p class="bg-2023-red shadow-md shadow-2023-bg text-white p-2.5 px-5 decoration-white text-lg border border-solid border-black font-bold w-[165px] absolute -top-[30px] left-[calc(50% - 86.5px)]">Contacta-nos!</p>
