ddd97d48d128b2d3658f5bdda0b7263850ee98ad
pedrosilva17 <pedroafsuper@gmail.com>
2023-08-27 02:26:13 +0100

    First iteration of navbar

--- resources/js/Components/Dropdown.vue
+++ resources/js/Components/Dropdown.vue
@@ -8,9 +8,9 @@ interface Props {
 }
 
 const props = withDefaults(defineProps<Props>(), {
-    align: "right",
-    width: "48",
-    contentClasses: () => ["py-1", "bg-white dark:bg-gray-700"],
+    align: 'right',
+    width: '48',
+    contentClasses: () => ['bg-2023-red-dark', 'text-center'],
 });
 
 let open = ref(false);
@@ -24,47 +24,51 @@ onUnmounted(() => document.removeEventListener("keydown", closeOnEscape));
 
 const widthClass = computed(() => {
     return {
-        "48": "w-48",
+        '20': 'w-20',
+        '32': 'w-32',
+        '48': 'w-48',
     }[props.width.toString()];
 });
 
 const alignmentClasses = computed(() => {
     if (props.align === "left") return "origin-top-left left-0";
 
-    if (props.align === "right") return "origin-top-right right-0";
+    if (props.align === 'center') {
+        return 'origin-top left-1/2 -translate-x-1/2';
+    }
+
+    if (props.align === 'right') {
+        return 'origin-top-right right-0';
+    }
 
     return "origin-top";
 });
 </script>
 
 <template>
-    <div class="relative">
+    <div class="relative justify-center">
         <div @click="open = !open">
             <slot name="trigger" />
         </div>
 
-        <!-- Full Screen Dropdown Overlay -->
-        <div v-show="open" class="fixed inset-0 z-40" @click="open = false" />
+        <div v-show="open" class="fixed z-40 justify-center" @click="open = false" />
 
         <transition
             enter-active-class="transition ease-out duration-200"
-            enter-from-class="transform opacity-0 scale-95"
-            enter-to-class="transform opacity-100 scale-100"
+            enter-from-class="transform opacity-0 -translate-y-6"
+            enter-to-class="transform opacity-100 translate-y-0"
             leave-active-class="transition ease-in duration-75"
-            leave-from-class="transform opacity-100 scale-100"
-            leave-to-class="transform opacity-0 scale-95"
+            leave-from-class="transform opacity-100 translate-y-0"
+            leave-to-class="transform opacity-0 -translate-y-6"
         >
             <div
                 v-show="open"
-                class="absolute z-50 mt-2 rounded-md shadow-lg"
+                class="absolute z-50"
                 :class="[widthClass, alignmentClasses]"
                 style="display: none"
                 @click="open = false"
             >
-                <div
-                    class="rounded-md ring-1 ring-black ring-opacity-5"
-                    :class="contentClasses"
-                >
+                <div :class="contentClasses">
                     <slot name="content" />
                 </div>
             </div>
--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -1,7 +1,19 @@
 <script setup lang="ts">
+<<<<<<< HEAD
 import { Head } from "@inertiajs/vue3";
 import Banner from "@/Components/Banner.vue";
 import Footer from "../Components/Footer.vue";
+=======
+import { ref } from 'vue';
+import { Head, Link, router } from '@inertiajs/vue3';
+import ApplicationMark from '@/Components/ApplicationMark.vue';
+import Banner from '@/Components/Banner.vue';
+import Dropdown from '@/Components/Dropdown.vue';
+import DropdownLink from '@/Components/DropdownLink.vue';
+import Navbar from '@/Components/Navbar.vue';
+import NavLink from '@/Components/NavLink.vue';
+import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
+>>>>>>> eb3c3a2 (First iteration of navbar)
 
 interface Props {
     title: string;
@@ -13,22 +25,10 @@ defineProps<Props>();
 <template>
     <div>
         <Head :title="title" />
-
         <Banner />
-
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            <!-- Page Heading -->
-            <header
-                v-if="$slots.header"
-                class="bg-white shadow dark:bg-gray-800"
-            >
-                <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                    <slot name="header" />
-                </div>
-            </header>
-
-            <!-- Page Content -->
-            <main class="bg-2023-bg">
+        <div class="min-h-screen bg-2023-bg">
+            <Navbar />
+            <main>
                 <slot />
             </main>
             <Footer></Footer>
