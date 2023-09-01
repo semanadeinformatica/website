35d563d27e61a5bac5f9f6f42d231ef0ad01d7bd
pedrosilva17 <pedroafsuper@gmail.com>
2023-09-02 00:00:14 +0100

    Almost done with mobile navbar revamp

--- resources/js/Components/ResponsiveNavLink.vue
+++ resources/js/Components/ResponsiveNavLink.vue
@@ -12,8 +12,8 @@ const props = defineProps<Props>();
 
 const classes = computed(() => {
     return props.active
-        ? 'block w-full h-fit py-8 bg-2023-teal lg:text-lg text-2023-bg text-center font-medium focus:outline-none transition duration-150 ease-in-out'
-        : 'block w-full h-fit py-8 bg-2023-bg hover:bg-2023-teal focus:bg-2023-teal text-2023-teal text-center hover:text-2023-bg focus:text-2023-bg lg:text-lg font-medium leading-5 focus:outline-none transition duration-150 ease-in-out';
+        ? 'flex flex-col w-full py-4 bg-2023-teal-dark underline text-2023-bg text-center font-medium focus:outline-none transition duration-150 ease-in-out'
+        : 'flex flex-col w-full py-4 bg-2023-teal-dark hover:underline focus:underline text-2023-bg text-center font-medium leading-5 focus:outline-none transition duration-150 ease-in-out';
 });
 </script>
 
