7ea2b81f83e24606c48a325ef1e9833806e7d1f5
Francisco <fmamp24@gmail.com>
2023-09-09 18:25:23 +0100

    very basic start of the competition page

---
+++ resources/js/Pages/Competition.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { Head, Link } from "@inertiajs/vue3";
+import AppLayout from "@/Layouts/AppLayout.vue";
+// import for podium (?)
+
+defineProps({
+    canLogin: Boolean,
+    canRegister: Boolean,
+    laravelVersion: String,
+    phpVersion: String,
+});
+
+</script>
+
+<template>
+    <Head title="Competition"/>
+    <AppLayout>
+
+    </AppLayout>
+</template>
