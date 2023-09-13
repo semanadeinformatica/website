6b500012fafe8bb936fe7b3ae7df89f34369014c
Francisco <fmamp24@gmail.com>
2023-09-13 13:28:49 +0100

    svg added, will refactor to grid

---
+++ resources/js/Components/Podium.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <section class="relative flex justify-center items-center py-10 ">
+        <img src="images/podium.svg" alt="podium" class="h-[80%] w-[80%]">
+    </section>
+</template>
--- resources/js/Pages/Competition.vue
+++ resources/js/Pages/Competition.vue
@@ -13,8 +13,50 @@ defineProps({
 </script>
 
 <template>
-    <Head title="Competition"/>
-    <AppLayout>
+    <Podium></Podium>
+    <AppLayout title="Competition">
+        <main class="bg-2023-bg">
 
+            <!-- RULES -->
+            
+            <section
+                class="relative flex top-[150px] grid grid-flow-row grid-cols-2 border-t border-black bg-2023-teal-dark text-justify max-lg:grid-flow-col max-lg:grid-cols-1 max-lg:grid-rows-2"
+            >
+                <h1
+                    class="absolute -top-[36px] left-[37%] border border-solid border-black bg-2023-red p-3 text-5xl font-bold text-white shadow-md shadow-2023-bg"
+                > Regulamento </h1>
+
+                <p class="p-20 text-lg text-white max-lg:pb-10">
+                    A Semana de Informática (SINF), organizada pelo Núcleo de
+                    Informática da Associação de Estudantes da Faculdade de
+                    Engenharia da Universidade do Porto (NIAEFEUP), foi criada
+                    com o intuito de permitir aos estudantes, independentemente
+                    do curso, desenvolver as suas capacidades nas diversas áreas
+                    da Informática, promovendo a sua interação com o mundo
+                    empresarial através de eventos sociais.
+                </p>
+                <p class="p-20 text-lg text-white max-lg:py-10">
+                    A edição de 2023 decorre entre os dias 25 e 31 de outubro.
+                    Os participantes têm a seu dispor palestras e workshops onde
+                    entrarão em contacto com diversas tecnologias e conceitos
+                    que não intervêm no percurso académico, focando-se portanto
+                    nas skills técnicas, assim como um pitch, sessões de
+                    entrevistas e visitas a empresas, fomentando o seu contacto
+                    com empresas de topo a nível nacional e internacional,
+                    podendo desenvolver as suas soft skills.
+                </p>
+            </section>
+
+            <!-- PARTICIPATE -->
+            <section class="relative flex  content-center items-center justify-center py-20">
+                <h2 class="absolute flex top-[15px] justify-center p-3 text-3xl font-bold text-2023-red">
+                    Vamos a isto?
+                </h2>
+
+                <button class="container mx-auto w-[180px] h-[50px] justify-center bg-2023-teal-dark border border-black text-white text-2xl text-center font-bold pb-2">
+                    <p>Participar!</p>
+                </button>
+            </section>
+        </main>
     </AppLayout>
 </template>
