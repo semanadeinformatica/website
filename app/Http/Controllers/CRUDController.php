a77b6375cf95a2d05da236decbff334980d972fd
toino <joaoapereira21@hotmail.com>
2023-09-13 23:23:32 +0100

    Add show and destroyed methods, document CRUD

--- app/Http/Controllers/CRUDController.php
+++ app/Http/Controllers/CRUDController.php
@@ -5,18 +5,51 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
+/**
+ * @template T
+ */
 abstract class CRUDController extends Controller
 {
+    /**
+     * The model class name.
+     * 
+     * @var class-string<T>
+     */
     protected string $model;
 
+    /**
+     * The Inertia view subfolder in the CRUD folder.
+     * 
+     * @var string
+     */
     protected string $view;
 
+    /**
+     * The validation rules.
+     * 
+     * @var array<int, string>
+     */
     protected array $rules = [];
 
-    protected array $storeRules;
+    /**
+     * The validation rules for the store method.
+     * 
+     * @var array<int, string>|null
+     */
+    protected ?array $storeRules;
 
-    protected array $updateRules;
+    /**
+     * The validation rules for the update method.
+     * 
+     * @var array<int, string>|null
+     */
+    protected ?array $updateRules;
 
+    /**
+     * The array to include with the views.
+     * 
+     * @return array<mixed, mixed>
+     */
     protected function with(): array
     {
         return [];
@@ -30,6 +63,18 @@ abstract class CRUDController extends Controller
 
         return Inertia::render("CRUD/$this->view/Index", [
             'items' => $items,
+            'with' => $with,
+        ]);
+    }
+
+    public function show($id)
+    {
+        $item = $this->model::find($id);
+
+        $with = $this->with();
+
+        return Inertia::render("CRUD/$this->view/Show", [
+            'item' => $item,
             'with' => $with,
         ]);
     }
@@ -55,6 +100,15 @@ abstract class CRUDController extends Controller
         ]);
     }
 
+    /**
+     * Gets called after the validation of the store method.
+     * The returned array will be used to create the new model,
+     * unless null is returned, in which case no model will be created.
+     *
+     * @param array<string, mixed> $new The validated values.
+     * 
+     * @return array<string, mixed>|null
+     */
     protected function created(array $new): ?array
     {
         return $new;
@@ -73,6 +127,16 @@ abstract class CRUDController extends Controller
         return redirect()->action([static::class, 'index']);
     }
 
+    /**
+     * Gets called after the validation of the update method.
+     * The returned array will be used to update the model, unless
+     * null is returned, in which case the model will not be updated.
+     * 
+     * @param array<string, mixed> $old The old values of the model.
+     * @param array<string, mixed> $new The validated values.
+     * 
+     * @return array<string, mixed>|null
+     */
     protected function updated(array $old, array $new): ?array
     {
         return $new;
@@ -93,9 +157,26 @@ abstract class CRUDController extends Controller
         return redirect()->action([static::class, 'index']);
     }
 
+    /**
+     * Gets called before the model is deleted.
+     * If true is returned, the model will be deleted.
+     * If false is returned, the model will not be deleted.
+     * 
+     * @param array<string, mixed> $old The old values of the model.
+     * 
+     * @return bool
+     */
+    protected function destroyed(array $old): bool
+    {
+        return true;
+    }
+
     public function destroy($id)
     {
-        $this->model::find($id)->delete();
+        $model = $this->model::find($id);
+        
+        if ($this->destroyed($model->toArray()))
+            $model->delete();
 
         return redirect()->back();
     }
