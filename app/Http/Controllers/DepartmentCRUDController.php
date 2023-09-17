7bd37df7979a4b164e43029675e7c92542c22d92
toni-santos <a.oliveirasantos122@gmail.com>
2023-09-17 15:58:28 +0100

    added support for search

--- app/Http/Controllers/DepartmentCRUDController.php
+++ app/Http/Controllers/DepartmentCRUDController.php
@@ -11,6 +11,8 @@ class DepartmentCRUDController extends CRUDController
 
     protected string $view = 'Department';
 
+    protected array $search = ['name'];
+
     protected array $rules = [
         'edition_id' => 'required|exists:editions,id',
         'name' => 'required|string',
