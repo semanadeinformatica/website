f8ca989c2cabaa179200f145d9c52c7af870e36a
Nuno Pereira <nunoafonso2002@gmail.com>
2023-09-17 04:08:35 +0100

    Improved controller validation

--- app/Http/Controllers/ProgramController.php
+++ app/Http/Controllers/ProgramController.php
@@ -9,25 +9,20 @@ class ProgramController extends Controller
 {
     public function show(Request $request)
     {
-        $validated = $request->validate([
-            'day' => 'required|integer|min:1',
-        ]);
-        $queryDay = $validated['day'];
-
         $edition = $request->input('edition');
 
         if ($edition === null) {
-
-            return;
+            return response('No edition found', 500);
         }
 
+        // We can do this here since we already fetched the edition from the DB, so we would not gain any performance from "short-circuiting" the validation
         $eventDays = $edition->event_days;
         $totalDays = count($eventDays);
 
-        if ($queryDay > $totalDays) {
-
-            // return;
-        }
+        $validated = $request->validate([
+            'day' => 'required|integer|min:1|max:'.$totalDays,
+        ]);
+        $queryDay = $validated['day'];
 
         $eventDay = $eventDays->get($queryDay - 1);
 
