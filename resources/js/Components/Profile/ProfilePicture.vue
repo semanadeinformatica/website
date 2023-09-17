3b35ed3bc5f7e0347a78f6daee7432340b70cc2c
pedronunes19 <pedromiguelmnunes@gmail.com>
2023-09-17 20:09:16 +0100

    update pfp

--- resources/js/Components/Profile/ProfilePicture.vue
+++ resources/js/Components/Profile/ProfilePicture.vue
@@ -1,11 +1,46 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import type { User } from "@/Types/User";
+import { useForm } from "@inertiajs/vue3";
+import route from "ziggy-js";
 
 interface Props {
     item: User | undefined;
 }
 
-defineProps<Props>();
+const props = defineProps<Props>();
+
+const form = useForm({
+    _method: "PUT",
+    name: props.item?.name,
+    email: props.item?.email,
+    photo: null as File | null,
+});
+
+const photoInput = ref<HTMLInputElement | null>(null);
+
+const selectNewPhoto = () => {
+    console.log(props.item?.profile_photo_url);
+    photoInput.value?.click();
+};
+
+const uploadNewPhoto = () => {
+    const photo = photoInput.value?.files?.[0];
+    if (photo) {
+        form.photo = photoInput.value?.files?.[0] ?? null
+        form.post(route('user-profile-information.update'), {
+            errorBag: "updateProfileInformation",
+            preserveScroll: true,
+            onSuccess: () => clearPhotoFileInput,
+        });
+    }
+};
+
+const clearPhotoFileInput = () => {
+    if (photoInput.value?.value) {
+        photoInput.value.value = "";
+    }
+};
 </script>
 
 <template>
@@ -15,11 +50,12 @@ defineProps<Props>();
         >
             <img
                 :src="item?.profile_photo_url"
-                alt="Profile Picture"
+                :alt="item?.name"
                 class="h-full w-full object-cover"
             />
         </div>
-        <button v-if="$page.props.auth.user.id == item?.id">
+        <input ref="photoInput" type="file" class="hidden" accept="image/*" @change="uploadNewPhoto" />
+        <button v-if="$page.props.auth.user.id == item?.id" @click.prevent="selectNewPhoto">
             <v-icon name="io-share" fill="2023-teal" scale="1.3"></v-icon>
         </button>
     </div>
