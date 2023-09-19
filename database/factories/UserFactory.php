b37d79608cdf9004e00cd50ba751760c04f82c30
toino <joaoapereira21@hotmail.com>
2023-09-19 17:14:07 +0100

    Fix user factories

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,6 +24,10 @@ class UserFactory extends Factory
     public function configure(): static
     {
         return $this->afterCreating(function (User $user) {
+            if ($user->usertype_id !== 0) {
+                return;
+            }
+
             $user->usertype()->associate($user->usertype_type::factory()->create([
                 'user_id' => $user->id,
             ]));
