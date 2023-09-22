2c95837454b538fdb67a0799e22efdbe5b475e36
toino <joaoapereira21@hotmail.com>
2023-09-22 22:09:15 +0100

    Make speakers a user type

---
+++ database/migrations/2023_09_22_192800_add_event_type_speaker_usertype.php
@@ -0,0 +1,47 @@
+<?php
+
+use App\Models\Event;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use function DatabaseHelpers\createManyToManyRelation;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('events', function (Blueprint $table) {
+
+        });
+
+        Schema::table('speakers', function (Blueprint $table) {
+            $table->foreignIdFor(User::class)->unique()->constrained()->cascadeOnDelete();
+            $table->dropColumn('name');
+            $table->dropConstrainedForeignIdFor(Event::class);
+        });
+
+        createManyToManyRelation(Event::class, User::class);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('events', function (Blueprint $table) {
+        });
+
+        Schema::table('speakers', function (Blueprint $table) {
+            $table->string('name');
+            $table->foreignIdFor(Event::class)->constrained()->cascadeOnDelete();
+            $table->dropConstrainedForeignIdFor(User::class);
+        });
+
+        Schema::dropIfExists('event_user');
+    }
+};
