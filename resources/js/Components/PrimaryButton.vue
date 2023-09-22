f8485f813d801add646402ea06a5eb94d3b9c732
pedrosilva17 <pedroafsuper@gmail.com>
2023-09-23 00:27:02 +0100

    Changed button properties, removed image

--- resources/js/Components/PrimaryButton.vue
+++ resources/js/Components/PrimaryButton.vue
@@ -4,7 +4,9 @@ import type { ButtonHTMLAttributes } from "vue";
 interface Props {
     type?: ButtonHTMLAttributes["type"];
     color?: string;
+    shadow?: string;
     textSize?: string;
+    padding?: string;
 }
 
 const buttonColor: Record<string, string> = {
@@ -15,18 +17,28 @@ const buttonColor: Record<string, string> = {
     teal: "bg-2023-teal",
 };
 
+const shadowColor: Record<string, string> = {
+    orange: "shadow-2023-orange",
+    "teal-dark": "shadow-2023-teal-dark",
+    "red-dark": "shadow-2023-red-dark",
+    red: "shadow-2023-red",
+    teal: "shadow-2023-teal",
+};
+
 withDefaults(defineProps<Props>(), {
     type: "submit",
     color: "teal",
+    shadow: "orange",
     textSize: "text-md",
+    padding: "",
 });
 </script>
 
 <template>
     <button
         :type="type"
-        class="text-md relative inline-flex items-center border border-black px-2 py-2 font-semibold text-white shadow-2023-orange transition-shadow hover:shadow-md active:shadow-none sm:px-4"
-        :class="[buttonColor[color], textSize]"
+        class="text-md relative inline-flex items-center border border-black px-2 py-2 font-semibold text-white transition-shadow hover:shadow-md active:shadow-none"
+        :class="[buttonColor[color], shadowColor[shadow], textSize, padding]"
     >
         <slot />
     </button>
