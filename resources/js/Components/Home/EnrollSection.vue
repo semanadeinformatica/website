a9cb1883c37ba9a63819434fd8820d590e31d429
pedrosilva17 <pedroafsuper@gmail.com>
2023-09-22 13:59:11 +0100

    Updated imports and passed backend values to component

--- resources/js/Components/Home/EnrollSection.vue
+++ resources/js/Components/Home/EnrollSection.vue
@@ -1,35 +1,42 @@
 <script setup lang="ts">
 import FancyButton from "@/Components/FancyButton.vue";
+
+interface Props {
+    days: number;
+    activities: number;
+    talks: number;
+    stands: number;
+}
+
+const { days, activities, talks, stands } = defineProps<Props>();
 </script>
 
 <template>
     <section
-        class="relative flex h-full flex-col pt-20 mx-10 mb-10 max-xs:items-center"
+        class="relative mx-10 mb-10 flex h-full flex-col pt-20 max-xs:items-center"
     >
         <h2
-            class="absolute self-center top-5 mr-[5px] sm:mr-[7px] border border-solid border-black bg-2023-red-dark px-2 py-2 text-xl font-bold text-white shadow sm:shadow-md shadow-2023-bg max-sm:relative max-sm:left-0 max-sm:top-6 sm:px-10 sm:py-6 sm:text-4xl"
+            class="absolute top-5 mr-[5px] self-center border border-solid border-black bg-2023-red-dark px-2 py-2 text-xl font-bold text-white shadow shadow-2023-bg max-sm:relative max-sm:left-0 max-sm:top-6 sm:mr-[7px] sm:px-10 sm:py-6 sm:text-4xl sm:shadow-md"
         >
             Tudo a postos?
         </h2>
         <div
-            class="mx-10 flex h-full flex-1 flex-col items-center border border-solid border-black bg-2023-bg p-3 sm:p-12 text-center font-bold text-white shadow-2xl shadow-2023-teal"
+            class="mx-10 flex h-full flex-1 flex-col items-center border border-solid border-black bg-2023-bg p-3 text-center font-bold text-white shadow-2xl shadow-2023-teal sm:p-12"
         >
             <p class="w-full pt-10 text-xl text-2023-teal sm:text-4xl">
                 A SINF está a tua espera!
             </p>
             <div
-                class="flex flex-row flex-wrap justify-center gap-4 p-3 sm:p-12 text-xl font-bold text-2023-teal max-lg:grid-cols-2 max-xs:grid-cols-1"
+                class="flex flex-row flex-wrap justify-center gap-4 p-3 text-xl font-bold text-2023-teal max-lg:grid-cols-2 max-xs:grid-cols-1 sm:p-12"
             >
-                <span class="text-center">7 dias</span>
-                <span class="text-center">70 bancas</span>
-                <span class="text-center">4 talks</span>
-                <span class="text-center">1 workshops</span>
+                <span class="text-center">{{ days }} dias</span>
+                <span class="text-center">{{ stands }} bancas</span>
+                <span class="text-center">{{ talks }} palestras</span>
+                <span class="text-center">{{ activities }} atividades</span>
             </div>
             <FancyButton> Inscrever-me </FancyButton>
             <div class="flex h-full w-full">
-                <picture
-                    class="mx-auto my-10 h-auto max-w-full object-none"
-                >
+                <picture class="mx-auto my-10 h-auto max-w-full object-none">
                     <source
                         srcset="https://picsum.photos/1500/500"
                         media="(min-width: 600px)"
