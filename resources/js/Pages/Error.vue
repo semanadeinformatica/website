5c7af4544d6e1a9a0025a0b1853011470f35cee2
toni-santos <a.oliveirasantos122@gmail.com>
2023-10-01 23:31:52 +0100

    error page styling and back button

--- resources/js/Pages/Error.vue
+++ resources/js/Pages/Error.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import { Head } from "@inertiajs/vue3";
 import { computed } from "vue";
 
 interface Props {
@@ -7,13 +9,9 @@ interface Props {
 
 const props = defineProps<Props>();
 
-const title = computed(() => {
-    return {
-        500: "500: Server Error",
-        404: "404: Page Not Found",
-        403: "403: Forbidden",
-    }[props.status];
-});
+const goBack = () => {
+    window.history.back();
+};
 
 const description = computed(() => {
     return {
@@ -25,8 +23,77 @@ const description = computed(() => {
 </script>
 
 <template>
-    <div>
-        <H1>{{ title }}</H1>
-        <div>{{ description }}</div>
-    </div>
+    <Head :title="props.status.toString()" />
+    <main class="min-h-screen bg-2023-bg">
+        <div
+            class="flex min-h-screen flex-col content-center items-center justify-center gap-44 py-16"
+        >
+            <img
+                id="svg-1"
+                class="absolute left-[7%] w-36 animate-2023-maintenance-jump opacity-50 max-ml:hidden"
+                src="/../images/cy-sinf.svg"
+                alt="Stylized SINF logo"
+            />
+            <img
+                id="svg-2"
+                class="absolute right-[7%] top-16 w-32 animate-2023-maintenance-jump opacity-50 max-ml:hidden"
+                src="/../images/rc-sinf.svg"
+                alt="Stylized SINF logo"
+            />
+            <img
+                id="svg-3"
+                class="absolute bottom-28 right-[15%] w-20 animate-2023-maintenance-jump opacity-50 max-ml:hidden"
+                src="/../images/oc-sinf.svg"
+                alt="Stylized SINF logo"
+            />
+
+            <div class="relative">
+                <img
+                    class="w-96 max-ml:w-72"
+                    src="/../images/sinf logo.png"
+                    alt="Stylized SINF logo"
+                />
+                <span
+                    class="margin-0 absolute -bottom-5 right-0 text-xl font-bold text-2023-teal"
+                    >2023</span
+                >
+            </div>
+
+            <div
+                class="flex w-fit flex-col items-center gap-7 text-center text-2023-teal-dark"
+            >
+                <h1 class="text-8xl">{{ props.status }}</h1>
+                <p>{{ description }}</p>
+                <PrimaryButton
+                    class="w-fit"
+                    text-size="text-xl"
+                    @click="goBack()"
+                >
+                    Regressar
+                </PrimaryButton>
+            </div>
+            <div class="text-center text-lg text-2023-teal-dark">
+                <p class="font-bold">Mais informações</p>
+                <p>geral@sinf.pt</p>
+            </div>
+        </div>
+    </main>
 </template>
+
+<style>
+body {
+    margin-bottom: 0px !important;
+}
+
+#svg-1 {
+    animation-delay: -0.5s;
+}
+
+#svg-2 {
+    animation-delay: -1s;
+}
+
+#svg-3 {
+    animation-delay: -0.8s;
+}
+</style>
