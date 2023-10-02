805c6141c40e7d92f998edea1295cce3fa94654e
toino <joaoapereira21@hotmail.com>
2023-10-02 03:46:12 +0100

    Fix triggers in migrations

--- database/migrations/2023_09_20_000156_create_quest_slots.php
+++ database/migrations/2023_09_20_000156_create_quest_slots.php
@@ -17,11 +17,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::dropIfExists('visit_history');
-        Schema::dropIfExists('update_visit_history');
-
-        Schema::dropIfExists('update_enrollment_points');
-        // Schema::dropIfExists('update_visit_history');
+        DB::unprepared('DROP TRIGGER IF EXISTS update_enrollment_points ON enrollment_quest;');
+        DB::unprepared('DROP FUNCTION IF EXISTS update_enrollment_points;');
 
         Schema::table('quests', function (Blueprint $table) {
             $table->dropColumn('points');
@@ -116,11 +113,11 @@ return new class extends Migration
     public function down(): void
     {
 
-        Schema::dropIfExists('update_enrollment_slot_history');
-        Schema::dropIfExists('update_enrollment_points');
+        DB::unprepared('DROP TRIGGER IF EXISTS update_enrollment_slot_history ON enrollment_slot;');
+        DB::unprepared('DROP FUNCTION IF EXISTS update_enrollment_points;');
 
-        Schema::dropIfExists('update_enrollment_quest_history');
-        Schema::dropIfExists('update_quest_slots');
+        DB::unprepared('DROP TRIGGER IF EXISTS update_enrollment_quest_history ON enrollment_quest;');
+        DB::unprepared('DROP FUNCTION IF EXISTS update_quest_slots;');
 
         Schema::dropIfExists('enrollment_slot');
         Schema::dropIfExists('quest_slot');
@@ -162,49 +159,5 @@ return new class extends Migration
             $table->foreignIdFor(\App\Models\Company::class)->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
-
-        //Triggers
-        DB::unprepared('
-                    CREATE OR REPLACE FUNCTION update_visit_history() RETURNS TRIGGER AS $$
-                        BEGIN
-                            DECLARE v_company_tier TEXT;
-                            DECLARE v_company_id INT;
-                            BEGIN
-                                IF (NEW IS NOT NULL) THEN
-        
-                                    SELECT c.tier
-                                    INTO v_company_tier
-                                    FROM quests q
-                                    LEFT JOIN companies c ON q.requirement_type = \'\App\Models\Company\' AND q.requirement_id = c.id
-                                    WHERE q.id = NEW.quest_id;
-        
-                                    IF ((v_company_tier != \'SILVER\') AND (SELECT requirement_type FROM quests WHERE id = NEW.quest_id) = \'\App\Models\Company\')THEN
-        
-                                        SELECT requirement_id INTO v_company_id FROM quests WHERE id = NEW.quest_id;
-        
-                                        INSERT INTO visit_history (participant_id, company_id, created_at, updated_at)
-                                        SELECT
-                                            e.participant_id,
-                                            v_company_id,
-                                            NOW(),
-                                            NOW()
-                                        FROM enrollments e
-                                        LEFT JOIN participants s ON e.participant_id = s.id
-                                        LEFT JOIN users u ON s.user_id = u.id
-                                        WHERE e.id = NEW.enrollment_id;
-                                    END IF;
-                                END IF;
-                            END;
-                            RETURN NEW;
-                        END;
-                    $$ LANGUAGE plpgsql;
-        
-            ');
-
-        DB::unprepared('
-                    CREATE TRIGGER update_visit_history
-                    AFTER INSERT ON enrollment_quest
-                    FOR EACH ROW EXECUTE FUNCTION update_visit_history();
-                ');
     }
 };
