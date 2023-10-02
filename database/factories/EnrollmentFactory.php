dc6117c61894475a6711c3ea64a0fa86e6bd8626
toino <joaoapereira21@hotmail.com>
2023-10-02 03:47:25 +0100

    Improve seeder, seed enrollments

---
+++ database/factories/EnrollmentFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Edition;
+use App\Models\Participant;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Enrollment>
+ */
+class EnrollmentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'edition_id' => Edition::factory(),
+            'participant_id' => Participant::factory(),
+        ];
+    }
+}
