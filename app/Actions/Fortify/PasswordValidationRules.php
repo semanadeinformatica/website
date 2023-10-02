91b883ec61d73f27c3270b78068c5ad8c0f50480
Nuno Pereira <nunoafonso2002@gmail.com>
2023-10-02 03:11:09 +0100

    Relaxed password restrictions

--- app/Actions/Fortify/PasswordValidationRules.php
+++ app/Actions/Fortify/PasswordValidationRules.php
@@ -13,6 +13,6 @@ trait PasswordValidationRules
      */
     protected function passwordRules(): array
     {
-        return ['required', 'string', (new Password)->length(8)->requireNumeric()->requireUppercase(), 'confirmed'];
+        return ['required', 'string', (new Password)->length(8), 'confirmed'];
     }
 }
