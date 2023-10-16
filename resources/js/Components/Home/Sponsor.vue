f046b07e6adf23656cd17d7760e4df747c7836cb
toni-santos <a.oliveirasantos122@gmail.com>
2023-10-16 17:27:08 +0100

    add scrolling to sponsor description

--- resources/js/Components/Home/Sponsor.vue
+++ resources/js/Components/Home/Sponsor.vue
@@ -51,7 +51,7 @@ defineProps<Props>();
             <span v-else class="text-xl font-bold underline">
                 {{ company?.name }}
             </span>
-            <p class="text-justify">{{ company?.usertype?.description }}</p>
+            <p class="text-justify  max-md:h-96 max-md:overflow-y-scroll">{{ company?.usertype?.description }}</p>
         </div>
     </VueFinalModal>
 </template>
