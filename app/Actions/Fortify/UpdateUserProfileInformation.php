99786faded503c7523067f8e089f811c89bd8691
Nuno Pereira <nunoafonso2002@gmail.com>
2023-10-18 19:00:20 +0100

    Fixed image size validation on user profile editing

--- app/Actions/Fortify/UpdateUserProfileInformation.php
+++ app/Actions/Fortify/UpdateUserProfileInformation.php
@@ -27,7 +27,7 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'email', 'max:255', Rule::unique('users')->ignore($user->id)],
-            'photo' => ['nullable', 'file', 'mimes:jpg,jpeg,png', 'max:10240'],
+            'photo' => ['nullable', File::types(['jpg', 'jpeg', 'png'])->max('10mb')],
             'cv' => ['nullable', File::types(['pdf'])->max('4mb')],
             'type' => 'sometimes|nullable|in:participant,company,admin,speaker',
             'title' => 'sometimes|nullable|string',
