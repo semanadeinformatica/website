29b367e962c26baefbd050bf30a8872038c344aa
toino <joaoapereira21@hotmail.com>
2023-10-19 15:12:32 +0100

    Fix dev script

--- dev.sh
+++ dev.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
 ./sail up -d laravel.test
-./sail artisan queue:work
-./sail npm run dev
+
+./sail artisan queue:work &
+worker=$!
+
+./sail npm run dev &
+vite=$!
+
+trap 'kill "$worker"; kill "$vite"; ./sail down' SIGINT
+while kill -s 0 "$worker" || kill -s 0 "$vite"; do
+    wait "$worker"
+    wait "$vite"
+done &>/dev/null
