53ed6d88bd31dab49b1d94f5c89c9ed66ecdc24f
Nuno Pereira <nunoafonso2002@gmail.com>
2023-10-23 22:45:29 +0100

    Added Portotech banner

---
+++ resources/js/Components/Footer/PortoTechBanner.vue
@@ -0,0 +1,11 @@
+<template>
+    <section class="mb-2 flex w-full justify-center p-5">
+        <a href="https://portotech.scaleupporto.pt/">
+            <img
+                src="/images/portotech-logo-blue.png"
+                alt="Porto Tech logo"
+                width="175"
+            />
+        </a>
+    </section>
+</template>
