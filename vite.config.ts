62d8c1136c2b7b5d583bd435e24ed0f6449c945a
Pedro Castro <falecompedroac@gmail.com>
2026-07-25 13:05:49 +0100

    migrated from postcss config to vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from "path";
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     publicDir: "public",
@@ -17,6 +18,7 @@ export default defineConfig({
         },
     },
     plugins: [
+        tailwindcss(),
         laravel({
             publicDirectory: "public",
             input: "resources/js/app.ts",
