523d654aa76f130e32acb6c9b38960cbe0c269cd
Pedro Castro <falecompedroac@gmail.com>
2026-07-27 15:09:30 +0100

    removed unused dependencies and updated others

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -31,6 +31,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
+            \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
         $middleware->redirectGuestsTo(fn () => route('login'));
