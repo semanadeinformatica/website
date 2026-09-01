e6497d6ca564ffb6d6c9a6cd06670c2a7d0b8e71
Pedro Monteiro <83165668+pedroafmonteiro@users.noreply.github.com>
2026-09-01 19:06:25 +0100

    chore: made the glowing orbs random

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -3,6 +3,8 @@ import { Head } from "@inertiajs/vue3";
 import Navbar from "@/Components/Navbar.vue";
 import Footer from "@/Components/Footer.vue";
 
+import GlowingOrbs from "@/Components/GlowingOrbs.vue";
+
 interface Props {
     title: string;
 }
@@ -14,26 +16,7 @@ defineProps<Props>();
     <div class="relative min-h-screen bg-black text-white">
         <Head :title="title" />
 
-        <div
-            class="pointer-events-none fixed inset-0 z-0 overflow-hidden"
-            aria-hidden="true"
-        >
-            <div
-                class="glow-orb-crimson-1 absolute top-[-20%] right-[-15%] h-[85vh] w-[80vw] rounded-[100%] blur-[70px] will-change-transform sm:blur-[100px]"
-            />
-
-            <div
-                class="glow-orb-purple-1 absolute bottom-[-20%] left-[-15%] h-[85vh] w-[80vw] rounded-[100%] blur-[70px] will-change-transform sm:blur-[100px]"
-            />
-
-            <div
-                class="glow-orb-crimson-2 absolute right-[-5%] bottom-[-10%] h-[65vh] w-[60vw] rounded-[100%] blur-[60px] will-change-transform sm:blur-[90px]"
-            />
-
-            <div
-                class="glow-orb-purple-2 absolute top-[-10%] left-[-5%] h-[65vh] w-[60vw] rounded-[100%] blur-[60px] will-change-transform sm:blur-[90px]"
-            />
-        </div>
+        <GlowingOrbs />
 
         <main class="relative z-10 min-h-screen">
             <Navbar />
--- resources/js/Pages/Maintenance.vue
+++ resources/js/Pages/Maintenance.vue
@@ -1,28 +1,13 @@
 <script setup lang="ts">
 import { Head } from "@inertiajs/vue3";
+import GlowingOrbs from "@/Components/GlowingOrbs.vue";
 </script>
 
 <template>
     <Head title="semana de informática 2026" />
 
     <div class="relative min-h-screen bg-black text-white">
-        <div
-            class="pointer-events-none fixed inset-0 z-0 overflow-hidden"
-            aria-hidden="true"
-        >
-            <div
-                class="glow-orb-crimson-1 absolute top-[-20%] right-[-15%] h-[85vh] w-[80vw] rounded-[100%] blur-[70px] will-change-transform sm:blur-[100px]"
-            />
-            <div
-                class="glow-orb-purple-1 absolute bottom-[-20%] left-[-15%] h-[85vh] w-[80vw] rounded-[100%] blur-[70px] will-change-transform sm:blur-[100px]"
-            />
-            <div
-                class="glow-orb-crimson-2 absolute right-[-5%] bottom-[-10%] h-[65vh] w-[60vw] rounded-[100%] blur-[60px] will-change-transform sm:blur-[90px]"
-            />
-            <div
-                class="glow-orb-purple-2 absolute top-[-10%] left-[-5%] h-[65vh] w-[60vw] rounded-[100%] blur-[60px] will-change-transform sm:blur-[90px]"
-            />
-        </div>
+        <GlowingOrbs />
 
         <main class="relative z-10 min-h-screen">
             <div
